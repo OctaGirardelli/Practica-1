c01b90db9eb362cf61ce8942e9f04f4e37442bd6
OctaGirardelli <ogirardelli@gmail.com>
2025-03-22 12:01:38 -0300

    Uso random.sample para evitar preguntas repetidas

--- questions.py
+++ questions.py
@@ -28,7 +28,7 @@ correct_answers_index = [1, 2, 0, 3, 1]
 puntaje = 0
 
 #Se eligen al azar 3 preguntas(en tuplas)
-questions_to_ask = random.choices(list(zip(questions, answers, correct_answers_index)), k=3)
+questions_to_ask = random.sample(list(zip(questions, answers, correct_answers_index)), k=3)
 
 # El usuario deberá contestar 3 preguntas
 for question in questions_to_ask:
